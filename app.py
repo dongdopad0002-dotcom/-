17cf6a5520b054f32ab037d814f225c5cc2a2bc9
dongdopad0002-dotcom <dongdopad0002@dge.go.kr>
2026-09-26 11:19:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,6 @@ def main() -> None:
         "run",
         "--global.developmentMode=false",
         str(app_file),
-        "--server.headless=true",
         "--server.address=127.0.0.1",
         "--server.port=8501",
         "--server.fileWatcherType=none",
